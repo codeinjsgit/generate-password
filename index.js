2100d98db2759dcab51236603ca92d7801e90a3d
Code in JavaScript <76252218+codeinjsgit@users.noreply.github.com>
2021-02-08 15:56:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const generatePassword = (length) => {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+/<=>?@[\]^_{|}~";
+    const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const specialCharacters = "!#$%&()*+/<=>?@[\]^_{|}~";
+    const charset = `${alphabets}${numbers}${specialCharacters}`;
     let password = "";
     for (let i = 0, n = charset.length; i < length; ++i) {
         password += charset.charAt(Math.floor(Math.random() * n));
     }
     return password;
 }
-console.log(generatePassword(10));  // k5oG!<2An^
+console.log(generatePassword(10));  // V4|j2>!USZ
