aaeb77a592bf94608a0627dfe843079d8f8cb557
Code in JavaScript <76252218+codeinjsgit@users.noreply.github.com>
2021-02-08 15:51:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const generatePassword = (length) => {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+/<=>?@[\]^_{|}~";
     let password = "";
     for (let i = 0, n = charset.length; i < length; ++i) {
         password += charset.charAt(Math.floor(Math.random() * n));
     }
     return password;
 }
-
-console.log(generatePassword(10));
+console.log(generatePassword(10));  // k5oG!<2An^
