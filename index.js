a8fdbcabe227847af5cb75c71213baa6d442ee19
Manoj Dharmadhikari <manoj.dharmadhikari@synechron.com>
2021-02-08 10:54:02 +0530

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+const generatePassword = (length) => {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return password;
+}
+
+console.log(generatePassword(10));
